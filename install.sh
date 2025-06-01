49e33b4e2777a31628b1101d3fb8c655d479b59a
Mattscreative <84041391+ryzendew@users.noreply.github.com>
2025-06-01 13:30:20 -0300

    fuck you jack again no room for you

--- install.sh
+++ install.sh
@@ -122,7 +122,7 @@ if pacman -Qs jack2 >/dev/null; then
 fi
 
 if ! pacman -Qs jack >/dev/null; then
-    print_status "Installing jack..."
+    print_status "Installing pipewire-jack..."
     sudo pacman -S --noconfirm piprewire-jack git
     print_success "jack installed successfully"
 fi
